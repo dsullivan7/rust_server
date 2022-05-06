4720f67738e10bb9d42ac942d5a0ce2c93d8d72e
Danny Sullivan <dbsullivan23@gmail.com>
2022-05-06 18:43:22 -0400

    adding auth library

--- src/authentication.rs
+++ src/authentication.rs
@@ -1,6 +1,11 @@
-use jsonwebtoken::jwk::AlgorithmParameters;
-use jsonwebtoken::{decode, decode_header, jwk, DecodingKey, Validation};
+use jsonwebtoken::{
+    decode, decode_header,
+    jwk::{AlgorithmParameters, JwkSet},
+    Algorithm, DecodingKey, Validation,
+};
 use serde::{Deserialize, Serialize};
+
+use derive_more::Display;
 
 #[derive(Clone, Serialize, Deserialize)]
 struct Claims {
@@ -8,36 +13,49 @@ struct Claims {
 }
 
 pub struct Authentication {
-    authority: String,
+    audience: String,
+    domain: String,
+}
+
+#[derive(Debug, Display)]
+pub enum AuthError {
+    #[display(fmt = "decode")]
+    Decode(jsonwebtoken::errors::Error),
+    #[display(fmt = "not_found")]
+    NotFound(String),
+    #[display(fmt = "unsupported_algorithm")]
+    UnsupportedAlgortithm(AlgorithmParameters),
+    #[display(fmt = "auth0_request_failed")]
+    RequestFailed(reqwest::Error),
 }
 
 impl Authentication {
-    pub async fn validate_token(&self, token: String) -> Result<Claims, String> {
-        let jwks = self
-            .get_jwks(&format!("https://{}/.well-known/jwks.json", self.authority))
-            .await;
-
-        let header = decode_header(&token).unwrap();
-        let kid = header.kid.unwrap();
-        let jwk = jwks.find(&kid).unwrap();
-
-        if let Some(j) = jwks.find(&kid) {
-            match j.algorithm {
-                AlgorithmParameters::RSA(ref rsa) => {
-                    let decoding_key = DecodingKey::from_rsa_components(&rsa.n, &rsa.e).unwrap();
-                    let mut validation = Validation::new(j.common.algorithm.unwrap());
-                    validation.validate_exp = false;
-                    let token = decode::<Claims>(&token, &decoding_key, &validation).unwrap();
-                    Ok(token.claims)
-                }
-                _ => unreachable!("this should be an RSA"),
+    pub async fn validate_token(&self, token: String) -> Result<Claims, AuthError> {
+        let header = decode_header(token.as_str()).map_err(AuthError::Decode)?;
+        let kid = header
+            .kid
+            .ok_or_else(|| AuthError::NotFound("kid not found in token header".to_string()))?;
+        let jwks: JwkSet = reqwest::get(&format!("https://{}/.well-known/jwks.json", self.domain))
+            .await
+            .map_err(AuthError::RequestFailed)?
+            .json()
+            .await
+            .map_err(AuthError::RequestFailed)?;
+        let jwk = jwks
+            .find(&kid)
+            .ok_or_else(|| AuthError::NotFound("No JWK found for kid".to_string()))?;
+        match jwk.clone().algorithm {
+            AlgorithmParameters::RSA(ref rsa) => {
+                let mut validation = Validation::new(Algorithm::RS256);
+                validation.set_audience(&[self.audience.as_str()]);
+                validation.set_issuer(&[format!("https://{}/", self.domain)]);
+                let key =
+                    DecodingKey::from_rsa_components(&rsa.n, &rsa.e).map_err(AuthError::Decode)?;
+                let token = decode::<Claims>(token.as_str(), &key, &validation)
+                    .map_err(AuthError::Decode)?;
+                Ok(token.claims)
             }
-        } else {
-            return Err("No matching JWK found for the given kid".into());
+            algorithm => Err(AuthError::UnsupportedAlgortithm(algorithm).into()),
         }
-    }
-
-    async fn get_jwks(&self, uri: &str) -> jwk::JwkSet {
-        reqwest::get(uri).await.unwrap().json().await.unwrap()
     }
 }
---
+++ src/extractors/claims.rs
@@ -0,0 +1,104 @@
+use crate::types::ErrorMessage;
+use actix_web::{
+    client::Client,
+    error::ResponseError,
+    http::{StatusCode, Uri},
+    Error, FromRequest, HttpResponse,
+};
+use actix_web_httpauth::{
+    extractors::bearer::BearerAuth, headers::www_authenticate::bearer::Bearer,
+};
+use derive_more::Display;
+use jsonwebtoken::{
+    decode, decode_header,
+    jwk::{AlgorithmParameters, JwkSet},
+    Algorithm, DecodingKey, Validation,
+};
+use serde::Deserialize;
+use std::{collections::HashSet, future::Future, pin::Pin};
+
+#[derive(Clone, Deserialize)]
+pub struct Auth0Config {
+    audience: String,
+    domain: String,
+}
+
+#[derive(Debug, Display)]
+pub enum ClientError {
+    #[display(fmt = "authentication")]
+    Authentication(actix_web_httpauth::extractors::AuthenticationError<Bearer>),
+    #[display(fmt = "decode")]
+    Decode(jsonwebtoken::errors::Error),
+    #[display(fmt = "not_found")]
+    NotFound(String),
+    #[display(fmt = "unsupported_algorithm")]
+    UnsupportedAlgortithm(AlgorithmParameters),
+}
+
+impl ResponseError for ClientError {
+    fn error_response(&self) -> HttpResponse {
+        match self {
+            Self::Authentication(_) => HttpResponse::Unauthorized().json(ErrorMessage {
+                error: None,
+                error_description: None,
+                message: "Requires authentication".to_string(),
+            }),
+            Self::Decode(_) => HttpResponse::Unauthorized().json(ErrorMessage {
+                error: Some("invalid_token".to_string()),
+                error_description: Some(
+                    "Authorization header value must follow this format: Bearer access-token"
+                        .to_string(),
+                ),
+                message: "Bad credentials".to_string(),
+            }),
+            Self::NotFound(msg) => HttpResponse::Unauthorized().json(ErrorMessage {
+                error: Some("invalid_token".to_string()),
+                error_description: Some(msg.to_string()),
+                message: "Bad credentials".to_string(),
+            }),
+            Self::UnsupportedAlgortithm(alg) => HttpResponse::Unauthorized().json(ErrorMessage {
+                error: Some("invalid_token".to_string()),
+                error_description: Some(format!(
+                    "Unsupported encryption algortithm expected RSA got {:?}",
+                    alg
+                )),
+                message: "Bad credentials".to_string(),
+            }),
+        }
+    }
+
+    fn status_code(&self) -> StatusCode {
+        StatusCode::UNAUTHORIZED
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Claims {
+    permissions: Option<HashSet<String>>,
+}
+
+impl Claims {
+    pub fn validate_permissions(&self, required_permissions: &HashSet<String>) -> bool {
+        self.permissions.as_ref().map_or(false, |permissions| permissions.is_superset(required_permissions))
+    }
+}
+
+impl FromRequest for Claims {
+    type Error = Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
+    type Config = ();
+
+    fn from_request(
+        req: &actix_web::HttpRequest,
+        _payload: &mut actix_web::dev::Payload,
+    ) -> Self::Future {
+        let authentication = &req.app_data::<AppState>().unwrap().authentication;
+        let extractor = BearerAuth::extract(req);
+        Box::pin(async move {
+            let credentials = extractor.await.map_err(ClientError::Authentication)?;
+            let token = credentials.token();
+            let claims = authentication.validate_token(token);
+            Ok(claims)
+        })
+    }
+}

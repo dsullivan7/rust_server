d7e92ddb9a09ce98cf7e266b81abd03ac7eac01c
Danny Sullivan <dbsullivan23@gmail.com>
2022-05-17 12:37:57 -0400

    adding debug statements for authentication

--- src/authentication.rs
+++ src/authentication.rs
@@ -1,3 +1,6 @@
+use anyhow::anyhow;
+use async_trait::async_trait;
+use derive_more::Display;
 use jsonwebtoken::{
     decode, decode_header,
     jwk::{AlgorithmParameters, JwkSet},
@@ -5,21 +8,18 @@ use jsonwebtoken::{
 };
 use serde::{Deserialize, Serialize};
 
-use derive_more::Display;
-
-use async_trait::async_trait;
 use mockall::*;
 
 #[derive(Debug, Display)]
 pub enum AuthError {
     #[display(fmt = "decode")]
-    Decode(jsonwebtoken::errors::Error),
+    Decode(anyhow::Error),
     #[display(fmt = "not_found")]
     NotFound(String),
     #[display(fmt = "unsupported_algorithm")]
     UnsupportedAlgortithm(AlgorithmParameters),
     #[display(fmt = "auth0_request_failed")]
-    RequestFailed(reqwest::Error),
+    RequestFailed(anyhow::Error),
 }
 
 #[derive(Clone, Serialize, Deserialize)]
@@ -42,43 +42,41 @@ pub struct Authentication {
 #[async_trait]
 impl IAuthentication for Authentication {
     async fn validate_token(&self, token: String) -> Result<Claims, AuthError> {
-        log::info!("validating token");
+        log::debug!("validating token");
 
-        let header = decode_header(token.as_str()).map_err(AuthError::Decode)?;
-        log::info!("found header");
+        let header =
+            decode_header(token.as_str()).map_err(|err| AuthError::Decode(anyhow!(err)))?;
+
+        log::debug!("found header");
         let kid = header
             .kid
             .ok_or_else(|| AuthError::NotFound("kid not found in token header".to_string()))?;
-        log::info!("found kid");
+        log::debug!("found kid");
         let jwks_endpoint = format!("https://{}/.well-known/jwks.json", self.domain);
-        log::info!("{}", jwks_endpoint);
+        log::debug!("{}", jwks_endpoint);
         let jwks: JwkSet = reqwest::get(&jwks_endpoint)
             .await
-            .map_err(|err| {
-                log::error!("request failed 1: {:?}", err);
-                AuthError::RequestFailed(err)
-            })?
+            .map_err(|err| AuthError::RequestFailed(anyhow!(err)))?
             .json()
             .await
-            .map_err(|err| {
-                log::error!("request failed 2: {:?}", err);
-                AuthError::RequestFailed(err)
-            })?;
-        log::info!("found jwks");
+            .map_err(|err| AuthError::RequestFailed(anyhow!(err)))?;
+        log::debug!("found jwks");
         let jwk = jwks
             .find(&kid)
             .ok_or_else(|| AuthError::NotFound("No JWK found for kid".to_string()))?;
-        log::info!("matched kid");
+        log::debug!("matched kid");
         match jwk.clone().algorithm {
             AlgorithmParameters::RSA(ref rsa) => {
                 let mut validation = Validation::new(Algorithm::RS256);
                 validation.set_audience(&[self.audience.as_str()]);
                 validation.set_issuer(&[format!("https://{}/", self.domain)]);
-                let key =
-                    DecodingKey::from_rsa_components(&rsa.n, &rsa.e).map_err(AuthError::Decode)?;
+                log::debug!("decoding key");
+                let key = DecodingKey::from_rsa_components(&rsa.n, &rsa.e)
+                    .map_err(|err| AuthError::Decode(anyhow!(err)))?;
+                log::debug!("decoded key");
                 let token = decode::<Claims>(token.as_str(), &key, &validation)
-                    .map_err(AuthError::Decode)?;
-                log::info!("found claims");
+                    .map_err(|err| AuthError::Decode(anyhow!(err)))?;
+                log::debug!("found claims");
                 Ok(token.claims)
             }
             algorithm => Err(AuthError::UnsupportedAlgortithm(algorithm).into()),

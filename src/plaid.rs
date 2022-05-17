a8f76d36815c35e94f5fb901c5618367487d37ef
Danny Sullivan <dbsullivan23@gmail.com>
2022-05-17 14:03:25 -0400

    finishing plaid implementation

--- src/plaid.rs
+++ src/plaid.rs
@@ -22,21 +22,18 @@ pub struct PlaidAccount {
     account_id: Option<String>,
     access_token: String,
 }
-pub struct PlaidProcessorToken {
-    value: String,
-}
 
 #[automock]
 #[async_trait]
 pub trait IPlaidClient: Send + Sync {
     async fn create_token(&self, user_id: String) -> Result<String, PlaidError>;
-    // async fn get_access_token(&self, public_token: String) -> Result<PlaidAccount, PlaidError>;
-    // async fn get_account(&self, account: PlaidAccount) -> Result<PlaidAccount, PlaidError>;
-    // async fn get_processor_token(
-    //     &self,
-    //     account: PlaidAccount,
-    //     accessor: String,
-    // ) -> Result<PlaidProcessorToken, PlaidError>;
+    async fn get_access_token(&self, public_token: String) -> Result<PlaidAccount, PlaidError>;
+    async fn get_account(&self, mut account: PlaidAccount) -> Result<PlaidAccount, PlaidError>;
+    async fn create_processor_token(
+        &self,
+        account: PlaidAccount,
+        accessor: String,
+    ) -> Result<String, PlaidError>;
 }
 
 pub struct PlaidClient {
@@ -116,5 +113,94 @@ impl IPlaidClient for PlaidClient {
             .as_str()
             .ok_or(PlaidError::FieldNotFound)?
             .to_owned())
+    }
+
+    async fn get_access_token(&self, public_token: String) -> Result<PlaidAccount, PlaidError> {
+        let res = self
+            .request(
+                reqwest::Method::POST,
+                "/item/public_token/exchange".to_string(),
+                Some(serde_json::json!({ "public_token": public_token })),
+            )
+            .await?;
+
+        let access_token = res["access_token"]
+            .as_str()
+            .ok_or(PlaidError::FieldNotFound)?
+            .to_owned();
+
+        let account = PlaidAccount {
+            name: None,
+            account_id: None,
+            access_token,
+        };
+
+        Ok(account)
+    }
+
+    async fn get_account(&self, mut account: PlaidAccount) -> Result<PlaidAccount, PlaidError> {
+        let account_res = self
+            .request(
+                reqwest::Method::POST,
+                "/accounts/get".to_string(),
+                Some(serde_json::json!({ "access_token": account.access_token })),
+            )
+            .await?;
+
+        let account_id = account_res["accounts"][0]["account_id"]
+            .as_str()
+            .ok_or(PlaidError::FieldNotFound)?
+            .to_owned();
+
+        let institution_id = account_res["item"]["institution_id"]
+            .as_str()
+            .ok_or(PlaidError::FieldNotFound)?
+            .to_owned();
+
+        let institution_res = self
+            .request(
+                reqwest::Method::POST,
+                "/institutions/get_by_id".to_string(),
+                Some(serde_json::json!({
+                    "institution_id": institution_id,
+                    "country_codes": ["US"],
+                })),
+            )
+            .await?;
+
+        let institution_name = institution_res["institution"]["name"]
+            .as_str()
+            .ok_or(PlaidError::FieldNotFound)?
+            .to_owned();
+
+        account.account_id = Some(account_id);
+        account.name = Some(institution_name);
+
+        Ok(account)
+    }
+
+    async fn create_processor_token(
+        &self,
+        account: PlaidAccount,
+        processor: String,
+    ) -> Result<String, PlaidError> {
+        let res = self
+            .request(
+                reqwest::Method::POST,
+                "/processor/token/create".to_string(),
+                Some(serde_json::json!({
+                    "access_token": account.access_token,
+                    "account_id":   account.account_id,
+                    "processor":    processor,
+                })),
+            )
+            .await?;
+
+        let processor_token = res["processor_token"]
+            .as_str()
+            .ok_or(PlaidError::FieldNotFound)?
+            .to_owned();
+
+        Ok(processor_token)
     }
 }

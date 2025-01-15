1dbd1a13d9d7692c08c36fc1f12ca4f3f3147bd3
Danny Sullivan <dbsullivan23@gmail.com>
2025-01-15 17:02:14 -0500

    adding connection to the database

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -1,14 +1,36 @@
+use axum::extract::State;
 use axum::Json;
 
+use sea_orm::EntityTrait;
 use serde::Serialize;
+
+use crate::errors::{self, ServerError};
+use crate::models::user::Entity as User;
+use anyhow::anyhow;
+
+use super::AppState;
 
 #[derive(Serialize)]
 pub struct UserRespose {
-    first_name: String,
+    first_name: Option<String>,
+    last_name: Option<String>,
 }
 
-pub async fn list_users() -> Json<Vec<UserRespose>> {
-    Json(vec![UserRespose {
-        first_name: "first_name".to_owned(),
-    }])
+pub async fn list_users(
+    State(state): State<AppState>,
+) -> Result<Json<Vec<UserRespose>>, ServerError> {
+    let conn = &state.conn;
+
+    let users: Vec<UserRespose> = User::find()
+        .all(conn)
+        .await
+        .map_err(|err| errors::ServerError::Internal(anyhow!(err)))?
+        .iter()
+        .map(|user| UserRespose {
+            first_name: user.first_name.to_owned(),
+            last_name: user.last_name.to_owned(),
+        })
+        .collect();
+
+    Ok(Json(users))
 }

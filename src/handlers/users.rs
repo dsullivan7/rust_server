93c5dc087ce1f359f5e710adf2683052c3708257
Danny Sullivan <dbsullivan23@gmail.com>
2025-01-16 13:47:41 -0500

    adding date and time to response

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -3,6 +3,7 @@ use axum::Json;
 
 use sea_orm::EntityTrait;
 use serde::Serialize;
+use uuid::Uuid;
 
 use crate::errors::{self, ServerError};
 use crate::models::user::Entity as User;
@@ -12,8 +13,11 @@ use super::AppState;
 
 #[derive(Serialize)]
 pub struct UserRespose {
+    user_id: Uuid,
     first_name: Option<String>,
     last_name: Option<String>,
+    created_at: chrono::DateTime<chrono::FixedOffset>,
+    updated_at: chrono::DateTime<chrono::FixedOffset>,
 }
 
 pub async fn list_users(
@@ -27,8 +31,11 @@ pub async fn list_users(
         .map_err(|err| errors::ServerError::Internal(anyhow!(err)))?
         .iter()
         .map(|user| UserRespose {
+            user_id: user.user_id.to_owned(),
             first_name: user.first_name.to_owned(),
             last_name: user.last_name.to_owned(),
+            created_at: user.created_at.to_owned(),
+            updated_at: user.updated_at.to_owned(),
         })
         .collect();
 

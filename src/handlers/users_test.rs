d8fa94f5d35bda52a09cf0b4361283f717ee8471
Danny Sullivan <dbsullivan23@gmail.com>
2025-01-23 11:32:01 -0500

    adding test for list users

--- src/handlers/users_test.rs
+++ src/handlers/users_test.rs
@@ -65,4 +65,69 @@ mod tests {
         assert_eq!(body.created_at, user_db.created_at);
         assert_eq!(body.updated_at, user_db.updated_at);
     }
+
+    #[tokio::test]
+    async fn test_list_users() {
+        let user_id_1 = Uuid::new_v4();
+        let user_id_2 = Uuid::new_v4();
+
+        let user_db_1: models::user::Model = models::user::Model {
+            user_id: user_id_1.to_owned(),
+            first_name: Some("first_name_1".to_owned()),
+            last_name: Some("last_name_1".to_owned()),
+            auth0_id: Some("auth0_id_1".to_owned()),
+            created_at: chrono::Utc::now().into(),
+            updated_at: chrono::Utc::now().into(),
+        };
+
+        let user_db_2: models::user::Model = models::user::Model {
+            user_id: user_id_2.to_owned(),
+            first_name: Some("first_name_2".to_owned()),
+            last_name: Some("last_name_2".to_owned()),
+            auth0_id: Some("auth0_id_2".to_owned()),
+            created_at: chrono::Utc::now().into(),
+            updated_at: chrono::Utc::now().into(),
+        };
+
+        let conn = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_results(vec![vec![user_db_1.clone(), user_db_2.clone()]])
+            .into_connection();
+
+        let auth = test_utils::get_default_auth();
+
+        let (default_auth_header, default_auth_header_value) =
+            test_utils::get_default_auth_header();
+
+        let router = router(AppState {
+            conn: Arc::new(conn),
+            authentication: Arc::from(auth),
+        });
+
+        let response = router
+            .oneshot(
+                Request::builder()
+                    .uri("/users")
+                    .header(default_auth_header, default_auth_header_value)
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Vec<UserRespose> = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body[0].user_id, user_id_1);
+        assert_eq!(body[0].first_name, Some("first_name_1".to_owned()));
+        assert_eq!(body[0].last_name, Some("last_name_1".to_owned()));
+        assert_eq!(body[0].created_at, user_db_1.created_at);
+        assert_eq!(body[0].updated_at, user_db_1.updated_at);
+
+        assert_eq!(body[1].user_id, user_id_2);
+        assert_eq!(body[1].first_name, Some("first_name_2".to_owned()));
+        assert_eq!(body[1].last_name, Some("last_name_2".to_owned()));
+        assert_eq!(body[1].created_at, user_db_2.created_at);
+        assert_eq!(body[1].updated_at, user_db_2.updated_at);
+    }
 }
